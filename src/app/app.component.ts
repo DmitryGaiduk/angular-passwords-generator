4506049648184d6d5b444b00f5ca66577885a66f
Dmitry Gaiduk <dgaiduk@kmilearning.com>
2023-01-15 23:45:04 +0100

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'password-generator';
+  password = ''
+  passwordLength = 0;
+
+  includeLetters = false;
+  includeSymbols = false;
+  includeNumbers = false;
+
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(e: any) {
+    let parsedNumber = parseInt(e.target.value)
+
+    if (!isNaN(parsedNumber)) {
+      this.passwordLength = parsedNumber;
+    }
+  }
+
+  generate(): void {
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const nums = '1234567890'
+    const symbols = '!@#$%^&*(){};+-=_'
+
+    let availableSymbols = '';
+    this.password = '';
+
+
+    if (this.includeSymbols) {
+      availableSymbols += letters
+    }
+
+    if (this.includeLetters) {
+      availableSymbols += symbols
+    }
+
+    if (this.includeNumbers) {
+      availableSymbols += nums;
+    }
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * availableSymbols.length);
+      this.password += availableSymbols[index];
+    }
+  }
 }
